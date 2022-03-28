1a175a4727c5884ceca9f54d43d888811fdca6bd
bbashcode <binbash.code@gmail.com>
2022-03-28 22:23:44 +0000

    page downloader node application

--- fetcher.js
+++ fetcher.js
@@ -1,18 +1,24 @@
-const fs = reqire("fs");
+const fs = require("fs");
 const request = require("request");
-
-
 let input = process.argv.slice(2);
+
 const url = input[0];
 const localFilePath = input[1];
 
-request(url, (error, response, body)=>{
-  fs.writeFile(localFilePath, (error) =>{
-    if(error){
-      return console.log(error);
-    } else {
-      console.log(`Downloaded and saved ${} bytes to ${localFilePath}`);
+request(url, (error, response, body) => {
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFile(localFilePath, body, error => {
+    if (error) {
+      console.error(error);
+      return;
     }
-  })
+    printAfterDownload();//file written successfully
+  });
+});
 
-})
+const printAfterDownload = function(){
+  console.log("Dowloaded and saved file!");
+};
+
